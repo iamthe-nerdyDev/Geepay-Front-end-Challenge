033ae1ee9a1e4e30553a7ef80b1a6ff5ef2b300e
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-01-27 04:23:17 +0100

    created components

---
+++ src/components/TopPlatforms/TopPlatforms.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopPlatforms: React.FC<{}> = () => {
+  return <div>TopPlatforms</div>;
+};
+
+export default TopPlatforms;
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Analytics } from "./Analytics";
+export { default as Graph } from "./Graph";
+export { default as Navbar } from "./Navbar";
+export { default as Orders } from "./Orders";
+export { default as Sidebar } from "./Sidebar";
+export { default as TopPlatforms } from "./TopPlatforms";
